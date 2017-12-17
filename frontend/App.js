3bb64766b9385d763da8c65b2b938aa6ec4c8aee
Jason Reeder <info@jasonreeder.com>
2017-12-17 12:29:42 -0800

    Add navbar

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Nav, NavItem, Navbar } from 'react-bootstrap/lib';
 import { Counter, Foods, Image } from './components';
+
+const NavbarComponent = () => (
+  <Navbar collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">wellio boilerplate</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem>First Nav</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 export const App = () => (
   <header>
-    <div>Webpack + React 16 + Flask-Webpack + Docker</div>
-    <Counter />
-    <Foods />
-    <Image />
+    <NavbarComponent />
+    <div className="container">
+      <div>Webpack + React 16 + Flask-Webpack + Docker</div>
+      <Counter />
+      <Foods />
+      <Image />
+    </div>
   </header>
 );
